d9ed0b3283f54698902620ebf63c5aa473cece01
Sazzad Sarker <sazzadsarker49@gmail.com>
2020-10-16 22:35:55 +0600

    increments

---
+++ increments.c
@@ -0,0 +1,10 @@
+#include<stdio.h>
+
+
+int main(int argc, char const *argv[])
+{
+    int a=5;
+    printf("The value of i++ is %d\n", a++);
+    printf("The value of ++i is %d\n", ++a);
+    return 0;
+}
