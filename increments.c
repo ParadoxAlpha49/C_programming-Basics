bc8519f5685b8bc5b769de72f883279087712198
Sazzad Sarker <sazzadsarker49@gmail.com>
2020-10-16 22:40:48 +0600

    with commnad line
    with commnad line

--- increments.c
+++ increments.c
@@ -4,7 +4,7 @@
 int main(int argc, char const *argv[])
 {
     int a=5;
-    printf("The value of i++ is %d\n", a++);
-    printf("The value of ++i is %d\n", ++a);
+    printf("The value of i++ is %d\n", a++); // post increment
+    printf("The value of ++i is %d\n", ++a); // pre increment
     return 0;
 }
