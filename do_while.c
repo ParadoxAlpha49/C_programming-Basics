0cac3f8a62501349037ab16c286d6f2e146c115c
Sazzad Sarker <sazzadsarker49@gmail.com>
2020-10-16 23:31:21 +0600

    do while loop

---
+++ do_while.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+
+int main(int argc, char const *argv[])
+{
+    int a=0;
+    do
+    {
+        printf("Enter the value of i is %d\n",a);
+        a++;
+    } while (a<10);
+    
+    return 0;
+}
